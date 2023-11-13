d3a6a4200fdbcc29ce579ee3c3b268f7d759089c
Filipe100 <filipe.thornhill@studytafensw.edu.au>
2023-11-13 12:32:33 +1100

    changes have been made

--- Program.cs
+++ Program.cs
@@ -7,6 +7,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<RoiDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+// Add after RoiDatabaseContext Service configuration
+
+// Define CORS policies to allow access to our API
+builder.Services.AddCors(options => {
+
+    // Build a default CORS policy
+    options.AddDefaultPolicy(policy => {
+
+        // Allow ANY origin to access our API
+        policy.AllowAnyOrigin();
+
+        // Allow SPECIFIC origins to access our API
+        //policy.WithOrigins("http://localhost:19006", "https://app.roi.com.au");
+
+        // Allow any HTTP header
+        policy.AllowAnyHeader();
+
+        // Allow any HTTP method
+        policy.AllowAnyMethod();
+
+    });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
