54e12aa49f493c72c6e3bc4e735a28a874ea938a
Filipe100 <filipe.thornhill@studytafensw.edu.au>
2023-11-13 11:43:12 +1100

    hellloooooooooooo

---
+++ Models/RoiDatabaseContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public partial class RoiDatabaseContext : DbContext
+{
+    public RoiDatabaseContext()
+    {
+    }
+
+    public RoiDatabaseContext(DbContextOptions<RoiDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departme__3214EC079A3A2A74");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__People__3214EC07E778D539");
+
+            entity.Property(e => e.City).HasMaxLength(255);
+            entity.Property(e => e.Country).HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.State).HasMaxLength(255);
+            entity.Property(e => e.Street).HasMaxLength(255);
+            entity.Property(e => e.Zip)
+                .HasMaxLength(10)
+                .HasColumnName("ZIP");
+
+            entity.HasOne(d => d.Department).WithMany(p => p.People)
+                .HasForeignKey(d => d.DepartmentId)
+                .HasConstraintName("FK__People__Departme__267ABA7A");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
